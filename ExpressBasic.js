07f119c4320c6f300ae0f17a79c802f3924dd362
Ankansa <ankan.sark@gmail.com>
2022-07-10 03:52:27 +0530

    ES7 Commit

---
+++ ExpressBasic.js
@@ -0,0 +1,28 @@
+// HTTP response status codes
+
+// HTTP response status codes indicate whether a specific HTTP request has been successfully completed. Responses are grouped in five classes:
+
+// 1.> Informational responses (100–199)
+// 2.> Successful responses (200–299)
+// 3.> Redirection messages (300–399)
+// 4.> Client error responses (400–499)
+// 5.> Server error responses (500–599)
+
+
+// Using HTTP Methods for RESTful Services
+
+// HTTP Verb	  CRUD	   Entire Collection (e.g. /customers)	       Specific Item (e.g. /customers/{id})
+
+// POST	        Create
+
+// GET	        Read
+
+// PUT	       Update/Replace
+
+// PATCH	   Update/Modify	
+
+// DELETE	   Delete
+
+
+//CORS IN EXPRESS
+// Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
